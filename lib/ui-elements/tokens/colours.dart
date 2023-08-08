27e6f67835e0b1ff50c657d5b9b5c5197b0c8938
Leonel Meque <leomeque@gmail.com>
2023-08-08 08:24:29 +0200

    feat: add tokens

---
+++ lib/ui-elements/tokens/colours.dart
@@ -0,0 +1,57 @@
+class Colours {
+  // primary
+  static const p500 = 0xFF3D3868;
+  static const p400 = 0xFF464077;
+  static const p300 = 0xFF645CAA;
+  static const p200 = 0xFF7E78B8;
+  static const p100 = 0xFFA5A0CE;
+  static const p75 = 0xFFBFBCDC;
+  static const p50 = 0xFFF0EFF7;
+
+  // secondary
+  static const s500 = 0xFF386d36;
+  static const s400 = 0xFF407d3e;
+  static const s300 = 0xFF5bb259;
+  static const s200 = 0xFF77bf75;
+  static const s100 = 0xFFa0d29f;
+  static const s75 = 0xFFbcdfbb;
+  static const s50 = 0xFFeff7ee;
+
+  // warning
+  static const w500 = 0xFF84712e;
+  static const w400 = 0xFF988235;
+  static const w300 = 0xFFd9ba4c;
+  static const w200 = 0xFFdfc66a;
+  static const w100 = 0xFFe9d797;
+  static const w75 = 0xFFefe3b6;
+  static const w50 = 0xFFfbf8ed;
+
+  // danger
+  static const d500 = 0xFF842e39;
+  static const d400 = 0xFF983541;
+  static const d300 = 0xFFd94c5d;
+  static const d200 = 0xFFdf6a79;
+  static const d100 = 0xFFe997a1;
+  static const d75 = 0xFFefb6bd;
+  static const d50 = 0xFFfbedef;
+
+  // greys
+  static const g500 = 0xFF0d0c10;
+  static const g400 = 0xFF0f0e12;
+  static const g300 = 0xFF15141a;
+  static const g200 = 0xFF3d3c41;
+  static const g100 = 0xFF77777a;
+  static const g75 = 0xFF9f9fa1;
+  static const g50 = 0xFFe8e8e8;
+
+  // neutral
+  static const dark = 0xFF15141A;
+  static const white = 0xFFFFFFFF;
+
+  // random
+  static const blue = 0xFFE0EEFD;
+  static const green = 0xFFE9FBF3;
+  static const orange = 0xFFFDEEE0;
+  static const pink = 0xFFFDE7F0;
+  static const yellow = 0xFFFAFBE9;
+}
---
+++ lib/ui-elements/tokens/spacing.dart
@@ -0,0 +1,11 @@
+class Spacing {
+  static double size64 = 64.0;
+  static double size56 = 56.0;
+  static double size48 = 48.0;
+  static double size40 = 40.0;
+  static double size32 = 32.0;
+  static double size24 = 24.0;
+  static double size16 = 16.0;
+  static double size8 = 8.0;
+  static double size4 = 4.0;
+}
